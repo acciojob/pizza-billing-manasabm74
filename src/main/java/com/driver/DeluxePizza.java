9ae35feb19b123142164c3d875da58f0c860a2ef
manasabm74 <manasabm47@gmail.com>
2024-05-08 17:52:51 +0530

    final commit

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,6 +4,11 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
+        //we call constructor of parent--super keyword is used
         // your code goes here
+        //delux pizza means pizza + extra cheese and
+        // topins so we nnedto call those funstions
+        addExtraCheese();
+        addExtraToppings();
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,29 +6,143 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    //we create variable
+    private int toppings;
+
+    //what we ave to take care off
+    boolean isExtraCheeseAdded;
+    boolean isExtrasToppingAdded;
+    boolean isBillCreated;
+
+    boolean isTakeAway;
+
+
+    public int getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(int toppings) {
+        this.toppings = toppings;
+    }
+
+    public boolean isExtraCheeseAdded() {
+        return isExtraCheeseAdded;
+    }
+
+    public void setExtraCheeseAdded(boolean extraCheeseAdded) {
+        isExtraCheeseAdded = extraCheeseAdded;
+    }
+
+    public boolean isExtrasToppingAdded() {
+        return isExtrasToppingAdded;
+    }
+
+    public void setExtrasToppingAdded(boolean extrasToppingAdded) {
+        isExtrasToppingAdded = extrasToppingAdded;
+    }
+
+    public boolean isBillCreated() {
+        return isBillCreated;
+    }
+
+    public void setBillCreated(boolean billCreated) {
+        isBillCreated = billCreated;
+    }
+
+    public boolean isTakeAway() {
+        return isTakeAway;
+    }
+
+    public void setTakeAway(boolean takeAway) {
+        isTakeAway = takeAway;
+    }
+
+
+
+    //once we create a variable we nned to initalize it
+    //boolean by default iniatiaze maeans thay said btdefault false;
+
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            this.price = 300;
+            this.toppings= 70;
+        }
+        else{
+            this.price = 400;
+            this.toppings = 120;
+        }
+
+        //boolean by default false,but iniatailze the is again agud practice
+        boolean isExtraCheeseAdded = false;
+        boolean isExtraToppingAdded = false;
+        boolean isBillCreated = false;
+        boolean isTakeAway = false;
+
+        //whwre we can gte exexact bill sseing baseprice
+        // right other are addupso
+
+        this.bill = "Base price of the pizza:" +this.price+"\n";
+
     }
 
     public int getPrice(){
+
         return this.price;
     }
 
     public void addExtraCheese(){
         // your code goes here
+        //if we write like this it add evrytime the chese so
+        if(!isExtraCheeseAdded) {
+            this.price += 80;
+            this.isExtraCheeseAdded = true;
+            //once we added we set this to true;
+        }
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!isExtrasToppingAdded){
+            //here insted of writing the topping pirice
+            // we can directly say this,toppings
+            //becasues we intilaize it in chese scction
+            //this.price += 120;
+
+            this.price += this.toppings;
+            isExtrasToppingAdded = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!isTakeAway){
+            this.price += 20;
+            isTakeAway = true;
+        }
     }
 
-    public String getBill(){
+    public String getBill() {
         // your code goes here
-        return this.bill;
+        if (!isBillCreated) {
+            if (isExtraCheeseAdded) {
+                this.bill += "Extra Cheese Added:80" + "\n";
+            }
+            if (isExtrasToppingAdded) {
+                this.bill += "Extra Toppings Added:" + this.toppings + "\n";
+            }
+            if (isTakeAway) {
+                this.bill += "Paperbag Added:20" + "\n";
+            }
+
+            this.bill += "Total Price" + this.price;
+            this.isBillCreated = true;
+
+            return this.bill;
+        }
+        return "";
     }
 }
